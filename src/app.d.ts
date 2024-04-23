329b6debc00107aa658f6df104d7c2cf7aeb5564
Sean <107402787+Pythonwithsean@users.noreply.github.com>
2024-04-23 12:36:58 +0100

    "Init"

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,5 +1,7 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
+//This Page is used for Declaring Global Types Variables, Interfaces
+//That i want to access through out my application 
 declare global {
 	namespace App {
 		// interface Error {}
