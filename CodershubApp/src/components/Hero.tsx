4a4cdb2bfc8b37fc201ffe8473e8a9a3fa10b249
Pythonwithsean <Pythonwithsean@gmail.com>
2024-05-04 14:58:14 +0100

    "Home Page 80% Done"

--- CodershubApp/src/components/Hero.tsx
+++ CodershubApp/src/components/Hero.tsx
@@ -1,7 +1,27 @@
 import { Typing } from "./Typing";
 import Navbar from "./Navbar";
+import { useState, useEffect } from "react";
 import { Container , Box } from "@mui/material";
 const Hero = () => {
+  const [showAnimation, setShowAnimation] = useState(false);
+
+  useEffect(() => {
+    function handleScroll() {
+      const scrollPosition = window.scrollY;
+      const triggerPosition = 600; // Adjust this value to set the scroll trigger point
+
+      if (scrollPosition > triggerPosition) {
+        setShowAnimation(true);
+      } else {
+        setShowAnimation(false);
+      }
+    }
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
+
   return ( 
     <>
       <Navbar />
@@ -27,15 +47,70 @@ const Hero = () => {
               <p className="paragraph">
               We offer <strong>1 to 1 Classes</strong> Python Data science,
               React and Next for Fullstack Development, Golang for Backend
-              Development
-            </p>
-            </Box>
-      </section>
-      <br />
-     </Container>
-
+              Development </p> </Box> </section> <br /> </Container> <br /><br /><br /><br />
      {/* ------------------------------------------------------------------------------------------ */}
      
+
+    <Container className={`container Semi-container ${showAnimation? "slide-in-left" : ""}`} style={
+      {
+        display: "flex",
+      }
+    }>
+
+    <img className="Hero-Img" src="../../public/SummerCoding Course.png" alt="coderhubUk" />
+    <Box component="section" sx={{ p: 2 }}>
+      <h1>
+        Summer Coding Course
+      </h1>
+      <p className="paragraph">
+        Our Summer Coding Course is a 6 week course that will help you learn
+        to code in Python, React, Next, Golang
+      </p>
+    </Box>
+
+      </Container>
+     <br />     
+     <br /><br /><br /><br />
+    <br />
+      <br />
+      {/* Logos of Languages They will learn */}
+      {/* I want spaces Between Containers to remove everything from beting jampacked */}
+      <Box>
+        <Container className={`container ${showAnimation? "slide-in-right" : ""}`}>
+          <h3>
+            Languages you will learn
+          </h3>
+          <Box component="section" sx={{ p: 2 }} style={{
+            display: "flex",
+            justifyContent: "space-around",
+            alignItems: "center",
+            flexWrap: "wrap",
+            margin: "auto",
+          }}>
+          <img  alt="TypeScript" width="60px" className="tech-stack-logo" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/go/go-original.svg"/>
+          <img  alt="TypeScript" width="60px" className="tech-stack-logo" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-plain.svg"/>
+          <img  alt="Java" width="60px" className="tech-stack-logo" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg"/> 
+          <img  alt="Python" width="50px" className="tech-stack-logo" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-plain.svg"/>
+          <img  alt="React" width="50px" className="tech-stack-logo" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg"/>
+          {/* Html,css, Javascript, Docker */}
+          
+          <img  alt="Html" width="50px" className="tech-stack-logo" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original-wordmark.svg"/>
+          <img  alt="Css" width="50px" className="tech-stack-logo" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original-wordmark.svg"/>
+          <img  alt="Javascript" width="50px" className="tech-stack-logo" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg"/>
+          <img  alt="Docker" width="50px" className="tech-stack-logo" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-original-wordmark.svg"/>
+
+      
+
+          </Box>
+        </Container>
+
+      </Box>
+      <br />
+      <br /><br /><br /><br /><br />
+
+
+
+     {/* ----------------------------------------------------------------------------------------------     */}
       <Container className="container" style={{}}>
         <h1>
           Why <span className="red-word">CodersHub</span> UK
@@ -77,9 +152,21 @@ const Hero = () => {
             Development
           </p>
         </Box>
+    </Container>
+    <br />
+    <br /><br /><br /><br />
+    <Container className="container">
+      <h1>
+        Our <span className="red-word">Tutors</span>
+      </h1>
+      <Box component="section" sx={{ p: 2}}>
+        <p className="paragraph">
+          Our tutors are experts in their fields and have years of experience
+          in teaching coding to students of all levels.
+        </p>
+      </Box>
+    </Container>
 
-
-      </Container>
     </>
   );
 };
