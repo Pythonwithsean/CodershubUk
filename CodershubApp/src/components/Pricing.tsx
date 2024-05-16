995b79579b6d8ca01f0b571be29975b941b722e8
Pythonwithsean <Pythonwithsean@gmail.com>
2024-05-16 14:12:32 +0100

    "Commit"

--- CodershubApp/src/components/Pricing.tsx
+++ CodershubApp/src/components/Pricing.tsx
@@ -4,14 +4,15 @@ import Navbar from "./Navbar";
 export default function Pricing() { 
 	return <Container className="container"> 
 	<Navbar/>
-	<br /><br /><br /><br />
-				<Box component="section" sx={{ p: 2 }}>
-					<h1>Get Ahead of Others Today!</h1>
-					<h3>Choose the best plan for your needs</h3>
-					<div>
-					</div>
-        </Box>
-
-	
+	<br />
+	<br />
+	<br />
+	<br />
+	<Box component="section" sx={{ p: 2 }}>
+	<h1>Get Ahead of Others Today!</h1>
+	<h3>Choose the best plan for your needs</h3>
+	<div>
+	</div>
+  </Box>	
 		</Container>
 }
