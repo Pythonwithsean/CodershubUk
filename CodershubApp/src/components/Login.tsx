a04eca25fd819bf33b58b8bd16dbc60a9ec769e2
Pythonwithsean <Pythonwithsean@gmail.com>
2024-05-16 11:35:15 +0100

    Adding Signup Page

--- CodershubApp/src/components/Login.tsx
+++ CodershubApp/src/components/Login.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
-import { Session, createClient } from '@supabase/supabase-js'
-import { Auth } from '@supabase/auth-ui-react'
+import { Link } from "react-router-dom"
+import { Session, createClient } from '@supabase/supabase-js' // import { Auth } from '@supabase/auth-ui-react'
 import "../styles/login.css"
 const PROJECT_URL = "https://talvpjiamafodpwvpkqz.supabase.co"
 const PROJECT_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRhbHZwamlhbWFmb2Rwd3Zwa3F6Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcxMzg2NzQ1MCwiZXhwIjoyMDI5NDQzNDUwfQ.4aAAYjVkQwBknWxZEih8g8DHwHMuerNYas7SVAX0VSg"
@@ -35,24 +35,23 @@ export default function Login({ children }: loginProps) {
         <div className='login-semicontainer' style={{ padding: '20px', backgroundColor: 'white', borderRadius: '10px' }}>
           <div>
             <h1>Login Component</h1>
+            <Link to="/signup">Signup</Link>
           </div>
-          <Auth
-            supabaseClient={supabase}
-            providers={['google', 'github']}
-            appearance={{
-              style: {
-                button: { background: 'black', color: 'white' },
-                container: { backgroundColor: 'white' },
-                anchor: { color: 'black' },
-                label: { color: 'black' },
-                input: { borderColor: 'black' },
-                loader: { color: 'black' },
-                message: { color: 'red' },
-              },
-            }}
-          />
-
-
+          {/* <Auth */}
+          {/*   supabaseClient={supabase} */}
+          {/*   providers={['google', 'github']} */}
+          {/*   appearance={{ */}
+          {/*     style: { */}
+          {/*       button: { background: 'black', color: 'white' }, */}
+          {/*       container: { backgroundColor: 'white' }, */}
+          {/*       anchor: { color: 'black' }, */}
+          {/*       label: { color: 'black' }, */}
+          {/*       input: { borderColor: 'black' }, */}
+          {/*       loader: { color: 'black' }, */}
+          {/*       message: { color: 'red' }, */}
+          {/*     }, */}
+          {/*   }} */}
+          {/* /> */}
         </div>
       </div>)
   }
