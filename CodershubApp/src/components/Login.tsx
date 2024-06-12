a09f77588a6ac3891372267456295104a8835d06
pythonwithsean <pythonwithsean@gmail.com>
2024-06-12 13:53:54 +0100

    "Commit"

--- CodershubApp/src/components/Login.tsx
+++ CodershubApp/src/components/Login.tsx
@@ -7,9 +7,12 @@ const PROJECT_URL = "https://talvpjiamafodpwvpkqz.supabase.co"
 const PROJECT_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRhbHZwamlhbWFmb2Rwd3Zwa3F6Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcxMzg2NzQ1MCwiZXhwIjoyMDI5NDQzNDUwfQ.4aAAYjVkQwBknWxZEih8g8DHwHMuerNYas7SVAX0VSg"
 const supabase = createClient(PROJECT_URL, PROJECT_TOKEN)
 
-function logout(): void {
-  supabase.auth.signOut()
-  localStorage.removeItem("sb-talvpjiamafodpwvpkqz-auth-token")
+async function logout(): Promise<void> {
+  let { error } = await  supabase.auth.signOut()
+  if (error != null){
+    console.error(error)
+    return 
+  }
   location.href = "/"
 }
 
