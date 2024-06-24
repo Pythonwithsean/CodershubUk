db1021229ccbf834687f7541197beb967502e1a3
pythonwithsean <pythonwithsean@gmail.com>
2024-06-24 13:36:22 +0100

    "Commit"

--- CodershubApp/src/components/SupabaseClient.ts
+++ CodershubApp/src/components/SupabaseClient.ts
@@ -1,4 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
-const PROJECT_URL = "https://talvpjiamafodpwvpkqz.supabase.co";
-const PROJECT_TOKEN ="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRhbHZwamlhbWFmb2Rwd3Zwa3F6Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcxMzg2NzQ1MCwiZXhwIjoyMDI5NDQzNDUwfQ.4aAAYjVkQwBknWxZEih8g8DHwHMuerNYas7SVAX0VSg";
-export const supabase = createClient(PROJECT_URL, PROJECT_TOKEN);
+import dotenv from "dotenv"
+dotenv.config()
+const PROJECT_URL = process.env.SUPABASE_PROJECT_URL
+const PROJECT_TOKEN = process.env.SUPABASE_TOKEN;
+export const supabase = createClient(PROJECT_URL!, PROJECT_TOKEN!);
