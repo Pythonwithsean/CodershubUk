c62703d554603b984bbca29dcbf7ed2ce3cba3f7
Pythonwithsean <pythonwithsean@gmail.com>
2024-12-24 11:15:31 +0000

    new style page

--- CodershubApp/src/components/ComingSoon.tsx
+++ CodershubApp/src/components/ComingSoon.tsx
@@ -1,9 +1,11 @@
+import "../styles/Comingsoon.css";
 export default function ComingSoon() {
   return (
     <div className="ComingSoon">
       <div className="content">
         <img src="/Logo.png" className="Logo" alt="Codershub" />
         <h1>Coming Soon 2025</h1>
+        <p>Learn by Coding, Building and Creating</p>
       </div>
     </div>
   );
